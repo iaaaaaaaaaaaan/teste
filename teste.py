4826fcf106dbf071860ea9ffea395d6d00c9a5d5
iaaaaaaaaaaaan <119634964+iaaaaaaaaaaaan@users.noreply.github.com>
2024-03-05 19:32:19 -0300

    Add files via upload

---
+++ teste.py
@@ -0,0 +1,7 @@
+a = 33
+b = "33"
+
+if b > a :
+    print("B é maior do que A")
+if b == a :
+    print("B é igual a A")
---
+++ teste2.py
@@ -0,0 +1,6 @@
+def my_function(fname):
+    print(fname + "RefSnes")
+
+my_function("emily")
+my_function("tobias")
+my_function("lucas")
